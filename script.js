6c91866b965b420fb2cea8ea2e78a6754d0b6537
nsrivastava999 <nsrivastava999@gmail.com>
2022-02-23 01:11:16 +0530

    Menu content displaying from json file

---
+++ script.js
@@ -0,0 +1,41 @@
+// fetch("./restaurant_content.json")
+//     .then(res => console.log(res));
+
+var sectionHTML = "";
+
+function getRestaurantData(){
+    fetch("./restaurant_content.json")
+        .then((res)=> {
+            return res.json();
+        }).then((data)=>{
+            // console.log(sectionData.menuSections.length);
+            var sectionData = data.menuSections;
+
+            for(let sectionIndex = 0; sectionIndex < sectionData.length; sectionIndex++){
+                // console.log(sectionData[sectionIndex]);
+                var currentSection = sectionData[sectionIndex];
+                sectionHTML += "<div class=\"menu-section\">";
+                sectionHTML += "<div class=\"menu-type\"> <h2 class=\"menu-header\" id=\""+currentSection.sectionId+"\">"+currentSection.sectionName;
+                sectionHTML += "</h2> <span class=\"menu-count\">"+ currentSection.sectionItems.length +" items</span> </div>"
+                // console.log(sectionHTML);
+                var sectionItems = currentSection.sectionItems;
+                for(let itemIndex = 0; itemIndex < sectionItems.length; itemIndex++){
+                    var currentItem = sectionItems[itemIndex];
+                    console.log(currentItem) 
+                    sectionHTML += "<div class=\"item-card\"> <div class=\"item-head\"> <div class=\"item-intro\">"
+                    sectionHTML += "<div class=\"item-name\">" + currentItem.itemName + "</div>"
+                    sectionHTML += "<div class=\"item-price\">" + currentItem.itemPrice + "</div> </div>"
+                    sectionHTML += "<div class=\"item-img\"> <img src=\"" + currentItem.itemImage + "\"> <a href=\"#\"><button>Add</button></a> </div> </div>"
+                    sectionHTML += "<div class=\"item-desc\"> <p>"+ currentItem.itemDesc +"</p> </div> </div>"
+                }
+                sectionHTML += "</div>";
+                console.log(sectionHTML);
+            }
+            document.querySelector(".middle-panel").innerHTML = sectionHTML;
+
+        }).catch(err => {
+            console.log(err);
+        })
+}
+
+getRestaurantData();
